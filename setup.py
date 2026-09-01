809e864aaa38d3f11acae4adabe52d33e0be73dd
Kris Surette <104463248+VTskier@users.noreply.github.com>
2026-09-01 11:36:42 -0400

    updated overflow trigger numbers and some review screen and also service info attachment code

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.RFApackage',
       version='2.0',
       description=('RFApackage'),
-      long_description='# docassemble.Rfapackage\r\n\r\nRFApackage\r\n\r\nAuthor: K. Surette, VTCourtForms / Legal Services Vermont\r\n\r\n\r\n',
+      long_description='# docassemble.Rfapackage\r\n\r\nRFApackage\r\n\r\nAuthors: \r\nK. Surette, VTCourtForms / Legal Services Vermont\r\nT. Fey, Lemma Legal\r\n\r\n\r\n',
       long_description_content_type='text/markdown',
       author='VTCourtForms by Legal Services Vermont',
       author_email='ksurette@legalservicesvt.org',
